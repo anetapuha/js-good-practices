10f59e0dccb6bebfffa3a74cec1704bf83f03ab6
anetapuha <morridog@hotmail.com>
2018-11-15 22:49:04 +1300

    unfinished just added the applyCSS function and helper function

--- we-need-love/myMumSaysImGoodPractice.js
+++ we-need-love/myMumSaysImGoodPractice.js
@@ -1,25 +1,33 @@
 var MenuStyler = (function(){
 
-  function _init(menuItems) {
-      /**
-       * Apply CSS styles to each menu item
-       */
-      for (let index = 0; index < menuItems.length; index++) {
-        menuItems[i].style.color = 'red';
-        menuItems[i].style.backgroundColor = 'blue';
-        menuItems[i].style.border = '2px solid #000';
-        menuItems[i].style.paddingLeft = '3px';
-        menuItems[i].style.marginTop = '3px';
-        menuItems[i].style.fontSize = '1.2em';
-        menuItems[i].style.fontStyle = 'italic';
+  function applyCSS(el, styles) {
+    for (var prop in styles) {
+      if (!styles.hasOwnProperty || styles.hasOwnProperty(prop)) {
+        el.style[prop] = styles[prop];
       }
+    }
+    return el;
+  }
+  function _init(menuItems) {
+      for (let index = 0; index < menuItems.length; index++) {
+       applyCSS(menuItems[index], {
+         color: 'red',
+         backgroundColor: 'blue',
+         border: '2px solid #000',
+         paddingLeft: '3px',
+         marginTop: '3px',
+         fontSize: '1.2em',
+         fontStyle: 'italic',
+       });
+      }   
   };
 
   return {
     init: function() {
-      var menuItems = document.getElementsByTagName('li');
+      let menuItems = document.getElementsByTagName('li');
       _init(menuItems);
     }
   }
 
 })();
+
