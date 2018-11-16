6d4e0afc3e5f1a5a4a5855592465ad92b125e61f
anetapuha <morridog@hotmail.com>
2018-11-16 18:12:07 +1300

    remove nested if check on submit button

--- we-need-love/youThoughtTheLastWasBad.js
+++ we-need-love/youThoughtTheLastWasBad.js
@@ -3,12 +3,6 @@ const adminUser = "admin123"
 const adminPassword = "milan12345"
 const isAdminUser = adminUser + adminPassword
 
-function Cancel() {
-    document.frm.username.value = "";
-    document.frm.password.value = "";
-    document.getElementById("username").focus();
-}
-
 function IsAdminLogin(name, password) {
     let user = name + password
 
@@ -23,6 +17,12 @@ function Login() {
     alert("You're logged in!");
 }
 
+function Cancel() {
+    document.frm.username.value = "";
+    document.frm.password.value = "";
+    document.getElementById("username").focus();
+}
+
 function _submit() {
     let f = document.frm;
     let username = f.username.value;
@@ -32,19 +32,19 @@ function _submit() {
     if(username == "" || password == "") {
         alert("Username and Password cannot be empty");
         userNameField.focus();
-        Cancel();
+         return Cancel();
     } else if(IsAdminLogin(username, password)) {
-        Login();
         alert("Welcome admin!");
+        return Login();
     } else {
-        Login()
+        return Login()
     } 
 }
 
 const submitButton = document.getElementsByTagName('button');
 
 if(typeof document.frm === 'object' && submitButton.length > 0) {
-        submitButton = submitButton[0]; 
+        let submitButton = submitButton[0]; 
         //Submit button click
         submitButton.addEventListener('click', function() {
             _submit();
